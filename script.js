ec00f7d3c885f070af1a7c14573feed2e49aa753
mgurabanidze92@gmail.com <mgurabanidze92@gmail.com>
2024-01-18 16:52:10 +0100

    Add login form functionality

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
-const container = document.querySelector(".categories"); 
+let blogCategoriesList = [];
 
-async function renderData() {
-    try {
-        const response = await fetch("http://localhost:3000/data");
-        const data = await response.json();
+var logInBtnElement = document.getElementById('log-in-btn');
+var logInFormElement = document.querySelector('.log-in-form');
+var closeBtnElement = document.getElementById('add');
+var mailInputElement = document.getElementById('mail');
 
-        
-        data.forEach(item => {
-            const newItem = document.createElement('div');
-            newItem.textContent = item.title; 
-            newItem.style.color = item.text_color;
-            container.appendChild(newItem);
-        });
+var submitBtnElement = document.querySelector('.mail-input button');
+var logInSuccessElement = document.querySelector('.log-in-success');
+var logInInputElement = document.querySelector('.log-in-input');
+var submitSuccessBtnElement = document.querySelector('.log-in-success button');
+var errorTextElement = document.querySelector('.error');
+var logInBgElement = document.querySelector('.log-in-form-bg');
 
-    } catch (error) {
-        console.error('Error fetching or rendering data:', error);
-    }
-}
+logInBtnElement.addEventListener('click', () => {
+    logInFormElement.removeAttribute('style');
+    logInBgElement.removeAttribute('style');
+})
 
-renderData();
+closeBtnElement.addEventListener('click', () => {
+    logInFormElement.setAttribute('style', 'display: none')
+    logInBgElement.setAttribute('style', 'display: none')
+})
 
+submitSuccessBtnElement.addEventListener('click', () => {
+    logInFormElement.setAttribute('style', 'display: none')
+    logInBgElement.setAttribute('style', 'display: none')
+})
+
+submitBtnElement.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(mailInputElement.value)
+    let formDataElement = new FormData();
+    formDataElement.append('email', mailInputElement.value)
+
+    fetch('https://api.blog.redberryinternship.ge/api/login', {
+        method: "POST",
+        headers: {
+            'accept': 'application/json',
+        },
+        body: formDataElement
+    }).then(res => {
+        if (res.status == 204) {
+            logInSuccessElement.removeAttribute('style');
+            logInInputElement.setAttribute('style', 'display: none');
+            logInBtnElement.setAttribute('style', 'display: none');
+            newBlogElement.removeAttribute('style');
+        } else {
+            errorTextElement.removeAttribute('style');
+            submitBtnElement.removeAttribute('style');
+        }
+        return res.status;
+    })
+        .then(data => {
+            console.log(data);
+            if (data == 204) {
+                localStorage.setItem('isLoggedIn', true)
+            }
+        })
+        .catch(error => console.error('Error:', error));
+})
